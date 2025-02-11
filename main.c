0261b312194aae7d6eafd45af9ac8f7365561fae
kurealnum <oscar.gaske.cs@gmail.com>
2025-02-10 20:23:55 -0500

    Kinda getting somewhere

--- main.c
+++ main.c
@@ -4,29 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define baseUrl "https://api.todoist.com/rest/v2/"
+
 int main(void) {
   printf("Current tasks\n");
   CURL *curl = curl_easy_init();
   if (curl) {
     // setup
-    char baseUrl[] = "https://api.todoist.com/rest/v2/";
     char *authToken = getenv("TODOIST_AUTH_TOKEN");
 
-    char authHeader[100] = "Authorization: Bearer";
-    strcat(authHeader, authToken);
-
-    char projectsUrl[100];
-    strcat(projectsUrl, strcat(baseUrl, projectsUrl));
-    if (authToken != NULL) {
-      curl_easy_setopt(curl, CURLOPT_HEADER, authHeader);
-    } else {
+    if (authToken == NULL) {
       return 1;
     }
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(curl, CURLOPT_URL, projectsUrl);
 
-    CURLcode currentTasks = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
+    char *authHeader = malloc(
+        (strlen("Authorization: Bearer ") + strlen(authToken)) * sizeof(char));
+    *authHeader = '\0';
+    strcat(authHeader, "Authorization: Bearer ");
+    strcat(authHeader, authToken);
+    printf("%s\n", authHeader);
+
+    /*char projectsUrl[100];*/
+    /*strcat(projectsUrl, strcat(baseUrl, projectsUrl));*/
+    /*if (authToken != NULL) {*/
+    /*  curl_easy_setopt(curl, CURLOPT_HEADER, authHeader);*/
+    /*} else {*/
+    /*  return 1;*/
+    /*}*/
+    /*curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");*/
+    /*curl_easy_setopt(curl, CURLOPT_URL, projectsUrl);*/
+    /**/
+    /*CURLcode currentTasks = curl_easy_perform(curl);*/
+    /*curl_easy_cleanup(curl);*/
+    free(authHeader);
   }
   curl_global_cleanup();
 }
